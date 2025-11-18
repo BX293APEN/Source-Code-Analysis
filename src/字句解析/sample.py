479fd33d31372f15f52882f661f0c4c30d066352
PEN <uemasaki20011222@gmail.com>
2025-11-18 09:35:17 +0900

    20251118

--- src/字句解析/sample.py
+++ src/字句解析/sample.py
@@ -1,4 +1,4 @@
-from ply import lex
+from ply import lex, yacc
 
 class LexicalAnalyze:
     """オブジェクトの説明
@@ -15,6 +15,10 @@ class LexicalAnalyze:
 
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
+        self.yacc = yacc.yacc(module=self, **kwargs)
+    
+    def parse(self): # yacc による構文解析
+        return self.yacc.parse(self.code, lexer=self.lexer)
     
     def get_tokens(self):
         self.lexer.input(self.code)
@@ -87,11 +91,69 @@ class LexicalAnalyze:
             t.type = "PRINT"
         return t
 
+    def p_statements_1(self, p):
+        "statements : statement statements"
+
+    def p_statements_2(self, p):
+        "statements : empty"
+
+    def p_empty_1(self, p):
+        "empty : "
+
+    def p_statement_1(self, p):
+        "statement : p_statement"
+
+    def p_statement_2(self, p):
+        "statement : a_statement"
+
+    def p_pstatement_1(self, p):
+        "p_statement : PRINT expression"
+
+    def p_astatement_1(self, p):
+        "a_statement : SYMBOL EQUAL expression"
+
+    def p_expression_1(self, p):
+        "expression : expression PLUS term"
+
+    def p_expression_2(self, p):
+        "expression : expression MINUS term"
+
+    def p_expression_3(self, p):
+        "expression : term"
+
+    def p_term_1(self, p):
+        "term : term TIMES factor"
+
+    def p_term_2(self, p):
+        "term : term DIVIDE factor"
+
+    def p_term_3(self, p):
+        "term : factor"
+
+    def p_factor_1(self, p):
+        "factor : num"
+
+    def p_factor_2(self, p):
+        "factor : MINUS num"
+    
+    def p_factor_3(self, p):
+        "factor : LPAREN expression RPAREN"
+
+    def p_num_1(self, p):
+        "num : NUMBER"
+
+    def p_num_2(self, p):
+        "num : SYMBOL"
+    
+
+    def p_error(self, p):
+        print("Syntax error at", p)
 
 if __name__ == "__main__":
     with open("main.txt", "r", encoding="UTF-8") as source:
         program = source.read()
 
     with LexicalAnalyze(program) as l:
-        print(l)
+        result = l.parse()
+        
     input()
